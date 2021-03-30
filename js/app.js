5f804bae4070c72d0f7b6e2e96f68f247862e67c
Arya Pranish Silwal <aryapranishsilwal@Aryas-MacBook-Pro.local>
2021-03-30 13:45:21 -0400

    Added new animations

--- js/app.js
+++ js/app.js
@@ -7,3 +7,25 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
+
+var revealElements = document.getElementsByClassName("picture");
+
+var controller = new ScrollMagic.Controller();
+
+for (var i = 0; i < revealElements.length; i++) {
+  // create a scene for each element
+  new ScrollMagic.Scene({
+    triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
+    offset: 50, // start a little later
+    duration: "80%",
+    triggerHook: 0.9,
+  })
+    .setClassToggle(revealElements[i], "show") // add class toggle
+    // .addIndicators({ name: "digit " + (i + 1) }) // add indicators (requires plugin)
+    .addTo(controller);
+}
+var scene = new ScrollMagic.Scene({
+  triggerElement: ".picture",
+})
+  .setClassToggle(".picture", "show")
+  .addTo(controller);
